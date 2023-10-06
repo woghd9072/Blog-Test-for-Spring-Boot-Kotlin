6f32416dfae03d9212e46d724e43e51a7cfde1da
jhshin <jhshin@dozn.co.kr>
2023-10-06 11:28:42 +0900

    add article controller

---
+++ src/main/kotlin/com/example/blog/HttpControllers.kt
@@ -0,0 +1,20 @@
+package com.example.blog
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping("/api/article")
+class ArticleController(private val repository: ArticleRepository) {
+
+  @GetMapping("/")
+  fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+  @GetMapping("/{slug}")
+  fun findOne(@PathVariable slug: String) = repository.findBySlug(slug)
+    ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+}
